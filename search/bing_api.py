c791d6fedaf2f3d0eef3b817a9fb69876f868593
Aditya Sathe <aditya@Adityas-MacBook-Pro.local>
2017-10-08 17:59:49 +0530

    bing displays results

--- search/bing_api.py
+++ search/bing_api.py
@@ -9,6 +9,7 @@ def search(text):
     response = requests.get(BASE_URL, params={'q': text}, headers=headers).json()
     result_list = response['webPages']['value']
     relevant_sentence_list = []
+    relevant_URL_list = []
 
     # Fetch various opinions and results from the search
     for res in result_list:
@@ -20,5 +21,23 @@ def search(text):
 
         # Append first 150 chars (ML Model limitation)
         relevant_sentence_list.append(norm_snip[:150].encode('utf-8'))
+
+    for res in result_list:
+        # Eliminate noise from sentence
+        urls = res['displayUrl'].lower()
+
+        # Normalizing the string to replace number expressed in words
+        # norm_snip = numtext_interconversion.wrapper_normalizer(noiseless_snip)
+
+        # Append first 150 chars (ML Model limitation)
+        relevant_URL_list.append(urls.encode('utf-8'))
+
+    print "Top 5 top hits:"
+    for i in range(5):
+        print 'Hit #{}:'.format(i+1)
+        print relevant_sentence_list[i]
+        print "URL:{}".format(relevant_URL_list[i])
+        print ""
+        print ""
 
     return relevant_sentence_list
